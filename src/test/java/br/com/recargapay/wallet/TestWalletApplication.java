95219123ec86104090246e1fc42144daa2958937
Antonio Amorim <afamorim@gmail.com>
2026-04-12 17:04:36 -0300

    Initial commit

---
+++ src/test/java/br/com/recargapay/wallet/TestWalletApplication.java
@@ -0,0 +1,11 @@
+package br.com.recargapay.wallet;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(WalletApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
